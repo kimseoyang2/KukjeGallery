bc29c6c0d7c4a0ff052916f26fe6c5ab3b198211
DylanHayunSong <dylanhayunsong@gmail.com>
2021-09-09 16:41:43 +0900

    Create WUC prefabs

---
+++ Assets/Scripts/UI/WUC_ClickPopup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WUC_ClickPopup : MonoBehaviour
+{
+    [SerializeField]
+    private Button btn;
+
+    [SerializeField]    
+    private int codeIndex;
+    /// <summary>
+    /// Index of art asset
+    /// </summary>
+    [SerializeField]
+    private int btnIndex;
+    /// <summary>
+    /// Index of button of art asset
+    /// </summary>
+
+    private void Awake ()
+    {
+        btn.onClick.AddListener(() => OnClickBtn(codeIndex, btnIndex));
+    }
+
+    private void OnClickBtn(int code, int btn)
+    {
+        Debug.Log(string.Format("JS Popup({0}, {1}) is called", code, btn));
+    }
+}
