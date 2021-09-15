b4d53218dfdd592459159312709389de4a744e3d
gimseoyang <61591951+kimseoyang2@users.noreply.github.com>
2021-09-15 14:28:51 +0900

    WebReqeust_v02

---
+++ Assets/MyFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Test()
+    {
+        Debug.Log("꺼짐");
+    }
+}
