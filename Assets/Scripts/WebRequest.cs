ce50f21758f36f83470e752a3816dfca0c4d5360
gimseoyang <61591951+kimseoyang2@users.noreply.github.com>
2021-09-16 10:33:50 +0900

    Webrequest_Revert

--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -1,33 +1,9 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.InteropServices;
 
 public class WebRequest : MonoBehaviour
 {
-
-    [SerializeField]
-    private int btnIndex;
-    [DllImport("__Internal")]
-    private static extern void Hello();
-
-    [DllImport("__Internal")]
-    private static extern void HelloString(string str);
-
-    [DllImport("__Internal")]
-    private static extern void paint_Popup(int btnIndex);
-
-
-    [DllImport("__Internal")]
-    private static extern void PrintFloatArray(float[] array, int size);
-
-    [DllImport("__Internal")]
-    private static extern int AddNumbers(int x, int y);
-
-    [DllImport("__Internal")]
-    private static extern string StringReturnValueFunction();
-
-    [DllImport("__Internal")]
-    private static extern void BindWebGLTexture(int texture);
 
     [SerializeField]
     private int codeIndex;
@@ -43,10 +19,8 @@ public class WebRequest : MonoBehaviour
         
     }
  
-    public void CallJSEvent()
+    public void CallJSEvent(int codeIndex)
     {
-
-        paint_Popup(btnIndex);
         Application.ExternalEval(string.Format("paint_Popup({0})", codeIndex));
         Debug.Log(string.Format("paint_Popup({0}) is called", codeIndex));
 
