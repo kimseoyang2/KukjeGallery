d87d74d33c9f0207036db447ceb5ea8bbf1ecb0d
Kimseoyang2 <jsn071577@gmail.com>
2021-09-23 00:24:53 +0900

    Assets Update

---
+++ Assets/Scripts/WUC_Audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class WUC_Audio : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button btn;
+
+
+    [SerializeField]
+    public UnityEvent onClickEvent;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        btn.onClick.AddListener(OnClickBtn);
+    }
+
+    private void OnClickBtn()
+    {
+        if (onClickEvent != null)
+        {
+            onClickEvent.Invoke();
+        }
+    }
+}
