50bfcc886a4c834f550f14a28133716befb21eef
Kimseoyang2 <jsn071577@gmail.com>
2021-09-22 21:17:19 +0900

    Delete Kiosk Texture(100MB+)

--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -5,32 +5,11 @@ using System.Runtime.InteropServices;
 public class WebRequest : MonoBehaviour
 {
 
-    [SerializeField]
-    private int btnIndex;
-    [DllImport("__Internal")]
-    private static extern void Hello();
-
-    [DllImport("__Internal")]
-    private static extern void HelloString(string str);
-
-    [DllImport("__Internal")]
-    private static extern void paint_Popup(int btnIndex);
-
-
-    [DllImport("__Internal")]
-    private static extern void PrintFloatArray(float[] array, int size);
-
-    [DllImport("__Internal")]
-    private static extern int AddNumbers(int x, int y);
-
-    [DllImport("__Internal")]
-    private static extern string StringReturnValueFunction();
-
-    [DllImport("__Internal")]
-    private static extern void BindWebGLTexture(int texture);
 
     [SerializeField]
     private int codeIndex;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,10 +22,16 @@ public class WebRequest : MonoBehaviour
         
     }
  
-    public void CallJSEvent()
+    //달 키오스크 영상 재생 함수
+    public void CallVidPlay(int codeIndex)
     {
-
-        paint_Popup(btnIndex);
+        Application.ExternalEval(string.Format("***({0})", codeIndex));
+        Debug.Log("키오스크 영상 재생");
+    }
+    //그림 상세보기 자바스크립트 호출함수
+    public void CallJSEvent(int codeIndex)
+    {
+        Application.ExternalEval(string.Format("paint_popup({0})", codeIndex));
         Debug.Log(string.Format("paint_Popup({0}) is called", codeIndex));
 
     }
