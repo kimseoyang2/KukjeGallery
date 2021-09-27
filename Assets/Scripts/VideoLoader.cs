792dc7e923578cd7b4d0e3bc60f91513a1052942
gimseoyang <61591951+kimseoyang2@users.noreply.github.com>
2021-09-27 18:49:16 +0900

    UI && Video Streaming Upload

---
+++ Assets/Scripts/VideoLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLoader : MonoBehaviour
+{
+    public string VideoName;
+    VideoPlayer vp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.url = System.IO.Path.Combine(Application.streamingAssetsPath, VideoName);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
