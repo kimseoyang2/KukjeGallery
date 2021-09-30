8bcf17749a2115e065bc0e8c89614a465a1bde5a
gimseoyang <61591951+kimseoyang2@users.noreply.github.com>
2021-09-30 14:13:52 +0900

    Interactive Video

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : SingletonBehavior<GameManager>
       
         #if !UNITY_EDITOR && UNITY_WEBGL
         // disable WebGLInput.captureAllKeyboardInput so elements in web page can handle keabord inputs
-        WebGLInput.captureAllKeyboardInput = false;
+        WebGLInput.captureAllKeyboardInput = true;
         #endif
         
     }
---
+++ Assets/VideoController_Forest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController_Forest : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject Screen;
+    public string VideoName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, VideoName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Screen.SetActive(true);
+            videoPlayer.Play();
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Screen.SetActive(false);
+            videoPlayer.Stop();
+
+        }
+    }
+
+
+}
