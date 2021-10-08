771c825bf6525c7b1037afae8056b99e0fa32963
gimseoyang <61591951+kimseoyang2@users.noreply.github.com>
2021-10-08 16:19:31 +0900

    Interactive ADD

--- Assets/Scripts/PictureViewPoint.cs
+++ Assets/Scripts/PictureViewPoint.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PictureViewPoint : MonoBehaviour
 {
     public WUC_TouchMove movePoint;
+    public WUC_TouchMove teleportPoint;
     public BGSoundManager bGSoundManager;
-
+    public bool isMove;
 
 
 
     [SerializeField]
     private Animator Dissolve;
 
+
+    public void Update()
+    {
+        if (isMove)
+        {
+            TeleportToPlayer();
+        }
+    }
     public void OnClick()
     {
         if (movePoint != null)
@@ -34,6 +43,22 @@ public class PictureViewPoint : MonoBehaviour
             bGSoundManager.BgmSoundOnOff(true);
             bGSoundManager.ChangeClip(0);
          
+        }
+    }
+
+    public void TeleportToPlayer()
+    {
+        if (teleportPoint != null)
+        {
+
+            Vector3 targetPos = teleportPoint.transform.position;
+            Vector3 targetEular = teleportPoint.transform.localEulerAngles;
+
+            GameManager.inst.MoveWUC_Touch(targetPos, targetEular);
+            MoveManager.inst.LookPic(this);
+
+
+
         }
     }
 }
